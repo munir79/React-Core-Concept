60259cae2ac8231162c0fc65ecdcb308dba88710
munir79 <jakirhossainmunir79@gmail.com>
2023-07-26 16:20:18 +0600

    important

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@
 //1 . think how create a page similar in component but diffrent in data  && its the primary concept
 
 import logo from './logo.svg';
+
 import './App.css';
 import './person.css';
+
 const number=222;
 const country={name:'Banglades',people:'18 core',Region:'Asia'}
 
 function App() {
+  const country1=['Bangladesh','India','Pakistan '];
+  // similar in loop and similar in data in object
+  const country3=[
+    {name:'Bangladesh',id:'201',region:'asia'},
+    {name:'Ameria',id:'201',region:'ameris'},
+    {name:'germanay',id:'203',region:'Europe'}
+  ]
   return (
     <div className="App">
       {number+677} <br />
@@ -15,6 +24,14 @@ function App() {
     {/* send data dynamically  */}
       <Person name='jakir HOssain' id='201' ></Person>  
       <Person  name='jakir HOssain munir ' id='202' ></Person>
+
+      {
+        country1.map(country=><Person name={country} ></Person>)
+      }
+
+      {
+      country3.map(r=><Country  name20={r.name} ></Country>)
+      }
      
     </div>
   );
@@ -23,12 +40,21 @@ function App() {
 export default App;
 
 // create Componenett with tag and Return in function app.
+// here is the main architecture loop similar in loop and diffrent in data 
 
 function Person(props){  
   return(
     <div className='person'>
       <h5> {props.name}   </h5>
       <h6>  {props.id}</h6>
+    </div>
+  )
+}
+
+function Country(props){
+  return(
+    <div>
+      name:{props.name20} Region:{}
     </div>
   )
 }
