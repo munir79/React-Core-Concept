f0f51183aa212d67bd887791f79a43255cf45cc3
munir79 <jakirhossainmunir79@gmail.com>
2023-07-26 15:40:59 +0600

    similar in component but diffrent in data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+
+//1 . think how create a page similar in component but diffrent in data  && its the primary concept
+
 import logo from './logo.svg';
 import './App.css';
+import './person.css';
 const number=222;
 const country={name:'Banglades',people:'18 core',Region:'Asia'}
 
@@ -8,7 +12,9 @@ function App() {
     <div className="App">
       {number+677} <br />
       Name:{country.name}  Pepole{country.people}
-      <Person></Person>
+    {/* send data dynamically  */}
+      <Person name='jakir HOssain' id='201' ></Person>  
+      <Person  name='jakir HOssain munir ' id='202' ></Person>
      
     </div>
   );
@@ -18,10 +24,13 @@ export default App;
 
 // create Componenett with tag and Return in function app.
 
-function Person(){  
+function Person(props){  
   return(
-    <div>
-      <h5> Jakir Hossain Munir </h5>
+    <div className='person'>
+      <h5> {props.name}   </h5>
+      <h6>  {props.id}</h6>
     </div>
   )
 }
+
+//3.Pass dynamic data to components, props in react
