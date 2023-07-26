7621719078e9606347e08270f887ce69bcb5b309
munir79 <jakirhossainmunir79@gmail.com>
2023-07-26 15:13:14 +0600

    compnent

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ function App() {
     <div className="App">
       {number+677} <br />
       Name:{country.name}  Pepole{country.people}
+      <Person></Person>
      
     </div>
   );
 }
 
 export default App;
+
+// create Componenett with tag and Return in function app.
+
+function Person(){  
+  return(
+    <div>
+      <h5> Jakir Hossain Munir </h5>
+    </div>
+  )
+}
